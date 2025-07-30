1f1884237533a70063a3df082e40ef06b6b522eb
Guilherme <guilhermehas@hotmail.com>
2025-07-30 10:47:54 +0200

    Added last test

--- keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
+++ keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
@@ -56,6 +56,15 @@ class SolJsonParserTest {
         Assertions.assertEquals(2, contractDeclaration.getFieldDeclarations().size());
     }
 
+
+    @Test
+    void parseContractWithBoth() throws IOException {
+        SolidityProgramElement programElement = getSolidityProgramElement("SimpleContract6.json");
+        Assertions.assertInstanceOf(ContractDeclaration.class, programElement);
+        ContractDeclaration contractDeclaration = (ContractDeclaration) programElement;
+        Assertions.assertEquals(2, contractDeclaration.getFieldDeclarations().size());
+    }
+
     private static SolidityProgramElement getSolidityProgramElement(String solFileName)
             throws IOException {
         SolJSONParser jsonParser = new SolJSONParser();
