916978a6bee9e69233778f673affcb20df6d3d25
guialvares <alvares@chalmers.se>
2025-08-28 15:59:07 +0200

    Added Array Declaration

---
+++ keyext.solidity.core/src/main/java/org/key_project/solidity/program/ast/declarations/ArrayDeclaration.java
@@ -0,0 +1,30 @@
+package org.key_project.solidity.program.ast.declarations;
+
+import org.key_project.logic.Name;
+import org.key_project.logic.SyntaxElement;
+
+public class ArrayDeclaration extends Declaration {
+    int length;
+    String struct;
+
+    public ArrayDeclaration(Name name, String struct, int length) {
+        super(name);
+        this.struct = struct;
+        this.length = length;
+    }
+
+    @Override
+    public SyntaxElement getChild(int n) {
+        return null;
+    }
+
+    @Override
+    public int getChildCount() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return struct + "[" + length + "]" + " memory " + name;
+    }
+}
