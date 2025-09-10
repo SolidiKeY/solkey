1f04de878ccab4f9ee77adba3f067060a804ea37
guialvares <alvares@chalmers.se>
2025-09-10 16:26:46 +0200

    Added mapping test

--- keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
+++ keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
@@ -575,6 +575,19 @@ class SolJsonParserTest {
         ContractDeclaration contractDec = getDeclStr(contract);
     }
 
+    @Disabled("Mapping type is not implemented")
+    @Test
+    void mapping() throws IOException {
+        //language=solidity
+        String contract = """
+                contract SimpleContract {
+                    mapping (bool => bool) b;
+                }""";
+        ContractDeclaration contractDec = getDeclStr(contract);
+        String contractS = contractDec.toString();
+        Assertions.assertTrue(contractS.contains(""));
+    }
+
     private static ContractDeclaration getDeclStr(String contract) throws IOException {
         final Path solc = Path.of("/opt", "local", "bin", "solc");
         SolcWrapper solcWrapper = new SolcWrapper(solc);
