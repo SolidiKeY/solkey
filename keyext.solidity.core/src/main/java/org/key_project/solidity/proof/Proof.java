c80bf27782017fb7befb6f49246ccf1565406685
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-09-26 15:52:32 +0200

    Added more infrastructure for parsing

---
+++ keyext.solidity.core/src/main/java/org/key_project/solidity/proof/Proof.java
@@ -0,0 +1,8 @@
+package org.key_project.solidity.proof;
+
+import org.key_project.prover.proof.ProofObject;
+
+///  TODO remove abstract and implement
+public abstract class Proof implements ProofObject {
+
+}
