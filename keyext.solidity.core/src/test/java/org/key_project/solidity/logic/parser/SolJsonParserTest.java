6e0179487ba129abe340dcb801aff461fc4b14e3
Guilherme <guilhermehas@hotmail.com>
2026-02-23 16:12:17 +0100

    renamed json parser test

--- keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
+++ keyext.solidity.core/src/test/java/org/key_project/solidity/logic/parser/SolJsonParserTest.java
@@ -34,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.key_project.solidity.program.ast.abstractions.PrimitiveType.INT256;
 
 
-public class SolJsonParserForTestingTest {
+public class SolJsonParserTest {
 
     @Test
     void parse() throws IOException {
