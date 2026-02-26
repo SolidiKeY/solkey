c7305053bb05da51fdfe6f356cd33536126ea196
Guilherme <guilhermehas@hotmail.com>
2026-02-27 00:20:45 +0100

    added schema variable visitor test

---
+++ keyext.solidity.core/src/test/java/org/key_project/solidity/program/ast/visitor/SchemaVariableVisitorTest.java
@@ -0,0 +1,31 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.key_project.solidity.program.ast.visitor;
+
+import org.key_project.logic.Name;
+import org.key_project.logic.op.sv.SchemaVariable;
+import org.key_project.solidity.rule.sv.ProgramSV;
+import org.key_project.util.collection.ImmutableList;
+import org.key_project.util.collection.ImmutableSLList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.key_project.solidity.rule.matching.inst.SVInstantiations.EMPTY_SVINSTANTIATIONS;
+import static org.key_project.solidity.rule.sv.SchemaVariableFactory.createProgramSV;
+
+class SchemaVariableVisitorTest {
+    @Test
+    void onlySchema() {
+        ProgramSV x = createProgramSV(new Name("x"), null, false);
+        ProgramSVCollector visitor =
+            new ProgramSVCollector(x, ImmutableSLList.nil(), EMPTY_SVINSTANTIATIONS);
+
+        visitor.start();
+        ImmutableList<SchemaVariable> schemaVars = visitor.getSchemaVariables();
+        assertEquals(1, schemaVars.size());
+        assertEquals(x, schemaVars.get(0));
+    }
+
+}
