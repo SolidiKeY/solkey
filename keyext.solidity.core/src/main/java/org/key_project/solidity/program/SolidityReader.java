01ebf0c50e3931336f214c329e5fd4175db9f0f1
Guilherme <guilhermehas@hotmail.com>
2026-02-26 14:17:25 +0100

    fixed Solidity Reader

--- keyext.solidity.core/src/main/java/org/key_project/solidity/program/SolidityReader.java
+++ keyext.solidity.core/src/main/java/org/key_project/solidity/program/SolidityReader.java
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package org.key_project.solidity.program;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.key_project.logic.Namespace;
 import org.key_project.solidity.common.Services;
 import org.key_project.solidity.logic.NamespaceSet;
@@ -10,6 +13,10 @@ import org.key_project.solidity.logic.SolidityBlock;
 import org.key_project.solidity.logic.op.ProgramVariable;
 
 import org.jspecify.annotations.NonNull;
+import org.key_project.solidity.parser.SolidityLexer;
+import org.key_project.solidity.parser.SolidityParser;
+import org.key_project.solidity.parser.SolidityToKeyConverter;
+import org.key_project.solidity.program.ast.statement.Block;
 
 public class SolidityReader {
     protected final Services services;
@@ -20,12 +27,33 @@ public class SolidityReader {
         this.nss = nss;
     }
 
+    static public SolidityParser parse(String s) {
+        CodePointCharStream input = CharStreams.fromString(s);
+
+        SolidityLexer lexer = new SolidityLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        return new SolidityParser(tokens);
+    }
+
+    static public SolidityParser.BlockContext parseBlockContext(String s) {
+        SolidityParser parser = parse(s);
+        return parser.block();
+    }
+
+    static public Block parseBlock(SolidityToKeyConverter stk, String s) {
+        SolidityParser.BlockContext bc = parseBlockContext(s);
+        return (Block) stk.visitBlock(bc);
+    }
+
     public SolidityBlock readBlockWithProgramVariables(
             Namespace<@NonNull ProgramVariable> programVariableNamespace, String solidity) {
-        throw new RuntimeException("Not implemented yet");
+        SolidityToKeyConverter stk = new SolidityToKeyConverter(services, programVariableNamespace, new Namespace<>());
+        return new SolidityBlock(parseBlock(stk, solidity));
     }
 
     public SolidityBlock readBlockWithEmptyContext(String solidity) {
-        throw new RuntimeException("Not implemented yet");
+        SolidityToKeyConverter stk = new SolidityToKeyConverter(services, new Namespace<>(), new Namespace<>());
+        return new SolidityBlock(parseBlock(stk, solidity));
     }
 }
