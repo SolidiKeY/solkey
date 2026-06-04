0dabf65f9e579b1c54525560a0e50c28d01aa9db
Richard Bubel <bubel@cs.tu-darmstadt.de>
2026-06-04 21:04:32 +0200

    Added classes required for server API

---
+++ keyext.solidity.core/src/main/java/org/key_project/solidity/util/KeYtherConstants.java
@@ -0,0 +1,6 @@
+package org.key_project.solidity.util;
+
+public interface KeYtherConstants {
+    String VERSION = "0.1.0-alpha";
+    String INTERNAL_VERSION = KeYResourceManager.getManager().getSHA1();
+}
